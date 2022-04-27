c43d6b589206532b3a6443c6ec758baa81e5ffbc
Julius Raynaldi <crazy.gentong@gmail.com>
2022-04-27 22:45:27 +0700

    custom ipfs

--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@ cd "`dirname $0`"
 source flags.sh
 cargo build --all --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/*.wasm ./res/
+
+near delete nft.junnon.testnet junnon.testnet 
+near create-account nft.junnon.testnet --masterAccount junnon.testnet --initialBalance 100
+
+near deploy nft.junnon.testnet --wasmFile res/non_fungible_token.wasm --initFunction new_default_meta --initArgs '{"owner_id":"junnon.testnet"}'
+
+near call nft.junnon.testnet nft_mint '{"receiver_id":"junnon.testnet"}' --accountId junnon.testnet --depositYocto  10000000000000000000000
+
--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -34,6 +34,7 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
+    counter: u128,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -64,11 +65,12 @@ impl Contract {
                 reference: None,
                 reference_hash: None,
             },
+            0,
         )
     }
 
     #[init]
-    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
+    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata, counter: u128) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
         Self {
@@ -80,6 +82,7 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            counter: counter,
         }
     }
 
@@ -94,11 +97,25 @@ impl Contract {
     #[payable]
     pub fn nft_mint(
         &mut self,
-        token_id: TokenId,
         receiver_id: ValidAccountId,
-        token_metadata: TokenMetadata,
     ) -> Token {
-        self.tokens.mint(token_id, receiver_id, Some(token_metadata))
+        let token_id = self.counter.clone();
+        let token_metadata:TokenMetadata = TokenMetadata{
+            title: Some("Olympus Mons".into()),
+            description: Some("The tallest mountain in the charted solar system".into()),
+            media: Some("https://gateway.pinata.cloud/ipfs/QmdsQ82YcEnKxqmo2TAPfDLmTZh8EnFbq6MkdtKdqMURBg/1.png".into()),
+            media_hash: None,
+            copies: Some(1u64),
+            issued_at: None,
+            expires_at: None,
+            starts_at: None,
+            updated_at: None,
+            extra: None,
+            reference: None,
+            reference_hash: None,
+        };
+        self.counter += 1;
+        self.tokens.mint(token_id.to_string(), receiver_id, Some(token_metadata))
     }
 }
 
